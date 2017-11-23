202f046ae4ab551ddc6fd56328210af47f4767d1
Amit Prasad <13373751+AmitPr@users.noreply.github.com>
2017-11-23 14:40:53 -0500

    Update Input.java
    equalsIgnoreCase is better for this

--- Input.java
+++ Input.java
@@ -3,7 +3,7 @@ public class Input {
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("you approach a crossroads.");
+		System.out.println("you approach crossroads.");
 		System.out.println("- left");
 		System.out.println("- right");
 		System.out.println();
@@ -11,10 +11,10 @@ public class Input {
 		System.out.print("input: ");
 		int dir = input.nextLn();
 
-		if(dir.equals("left")){
+		if(dir.equalsIgnoreCase("left")){
 			System.out.println("you decide to go left. along the way, you're");
 			System.out.println("stopped by a pack of orcs. they kill you. the end.");
-		} else if(dir.equals("right")){
+		} else if(dir.equalsIgnoreCase("right")){
 			System.out.println("you decide to head right. it begins to rain.");
 			System.out.println("the ground becomes really muddy and you just sink right in.");
 			System.out.println("the mud suffocates you. the end.");
