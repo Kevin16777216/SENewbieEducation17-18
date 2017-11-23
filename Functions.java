4a12c9053629cd0e148ece69416e68bf6789653b
Amit Prasad <13373751+AmitPr@users.noreply.github.com>
2017-11-23 14:38:05 -0500

    Optimized Prime method
    Only need to iterate to half of n + 1

--- Functions.java
+++ Functions.java
@@ -2,7 +2,7 @@ public class Functions {
 
 	public static boolean isPrime(int n) {
 
-		for(int i = 2; i < n; i++) {
+		for(int i = 2; i < (n/2+1); i++) {
 			if(n % i == 0) {
 				return false;
 			}
